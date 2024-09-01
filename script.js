b65dfa2d30fc5b06f16e116785e260bec225bad4
Sadia Khan <sadiakhann413@gmail.com>
2024-09-01 18:56:39 +0500

    scripting start

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// var questions = [
-//     {
-//         question : "What is  5 + 5 ?",
-//         Options : [2,10,6,3],
-//         Answer : 1
-//     },
+var questions = [
+    {
+        Question : "What is  5 + 5 ?",
+        Options : [2,10,6,3],
+        Answer : 1
+    },
+    {
+        Question: "what is 2 - 2",
+        Options: [2,8,0,3],
+        Answer: 2
+    }
 
-// ]
+]
